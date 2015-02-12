9a50baca7f027e670a3dfca4b7749dd7fb40b915
Julien Biezemans <jb@jbpros.com>
2015-02-12 16:52:16 +0100

    Revamp homepage
    The homepage has moved from the old legacy app to the "modern" app and
    is based on Bootstrap.
    
    Bootstrap which has been bumped to 3.3.2 in the process.
    
    Squashed commit of the following:
    
    commit 2bbb8c23db0288a5acae48e9b01cda92e8070c0e
    Author: Julien Biezemans <jb@jbpros.com>
    Date:   Thu Feb 12 16:51:31 2015 +0100
    
        First iteration done
    
    commit bc30f2cb003d02931ee87481179169f9110510bc
    Author: Julien Biezemans <jb@jbpros.com>
    Date:   Thu Feb 12 13:53:23 2015 +0100
    
        WIP

--- apps/legacy-cukes-info/app.rb
+++ apps/legacy-cukes-info/app.rb
@@ -4,8 +4,8 @@ module LegacyCukesInfo
   class App < Sinatra::Base
     set root: File.dirname(__FILE__)
 
-    get '/' do
-      send_file settings.root + '/public/index.html'
+    get '/legacy-index' do
+      send_file settings.root + '/public/legacy-index.html'
     end
   end
 end
