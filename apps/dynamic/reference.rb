edb2b9a3fb645ae7fd0827afcb7053ad6f67f088
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-02-19 15:34:23 +0000

    Some more content in the reference doc

--- apps/dynamic/reference.rb
+++ apps/dynamic/reference.rb
@@ -13,5 +13,83 @@ module Cucumber
       # in order with Nokogiri
       %Q{<h#{header_level} class="header">#{text}</h#{header_level}>}
     end
+
+    def postprocess(html)
+      doc = Nokogiri::HTML("<!DOCTYPE html>\n<html><body>#{html}</body></html>")
+
+      wrap_headers(doc)
+      create_anchors(doc)
+      nav_links = create_nav_links(doc)
+
+      doc.css('body').first.children.first.add_previous_sibling(nav_links)
+
+      doc.to_s
+    end
+
+    private
+
+    def wrap_headers(doc)
+      doc.css('.header').map do |node|
+        parent = doc.create_element('section')
+        node.add_previous_sibling(parent)
+
+        nodes = Nokogiri::XML::NodeSet.new(doc)
+        while next_node = node.next
+          break if next_node.name =~ /h\d/
+          nodes.push(node)
+          node = next_node
+        end
+
+        parent.add_child(nodes)
+      end
+    end
+
+    def create_anchors(doc)
+      doc.css('.header').map do |node|
+        anchor = anchorify(node.text)
+        anchor_tag = %Q{<a id="#{anchor}" class="reference-anchor" href="##{anchor}" aria-hidden="true"><span class="reference-link"></span></a>}
+        node.prepend_child(anchor_tag)
+      end
+    end
+
+    # Creates a nav (nested ul) with links to headers
+    def create_nav_links(doc)
+      result = doc.create_element('ul')
+      ul = result
+      li = nil
+      last_level = 1
+      doc.css('.header').map do |node|
+        anchor = anchorify(node.text)
+        level = node.name[1..-1].to_i
+        delta = level-last_level
+        if delta > 0
+          delta.times do
+            ul = doc.create_element('ul')
+            li.add_child(ul)
+          end
+        end
+        if delta < 0
+          delta.abs.times do
+            ul = ul.parent.parent
+          end
+        end
+
+        last_level = level
+
+        # TODO: Escape node.text
+        li = doc.parse(%Q{<li><a href="##{anchor}">#{node.text}</a></li>"}).first
+        ul.add_child(li)
+      end
+      result
+    end
+
+    def anchorify(string)
+      string
+      .gsub(/[']+/, '')
+      .gsub(/\W+/, ' ')
+      .strip
+      .downcase
+      .gsub(' ', '-')
+    end
   end
 end
--- apps/dynamic/views/reference.md
+++ apps/dynamic/views/reference.md
@@ -3,7 +3,7 @@ title: Reference
 slug: reference
 renderer: Cucumber::Reference
 ---
-# Cucumber Reference
+# Gherkin
 
 This is some ruby code:
 
@@ -12,3 +12,39 @@ def hello
   puts "您好"
 end
 ```
+
+## Scenarios
+
+Bla bla
+
+## Steps
+
+Bla bla
+
+## Background
+
+Bla bla
+
+## Tags
+
+Bla bla
+
+# Cucumber API
+
+Bla bla
+
+## Step Definitions
+
+Bla bla
+
+## Hooks
+
+Bla bla
+
+### Tagged Hooks
+
+Bla bla
+
+# Command line
+
+Bla bla
