68440ad4e25050183021f0ad4ce54010d6bcd0bb
Julien Biezemans <jb@jbpros.com>
2015-02-27 15:31:47 +0100

    Move homepage to dynamic app

--- apps/modern/jekyll-content/_site/js/config.js
+++
@@ -1,4 +0,0 @@
-window.stripeConfig = {
-  publishableKey: 'pk_live_CM7KruXBBpWbcX4YezpjncEm',
-  chargeUrl: 'https://customers.cucumber.pro/subscriptions'
-}
--- apps/modern/jekyll-content/js/config.js
+++
@@ -1,6 +0,0 @@
----
----
-window.stripeConfig = {
-  publishableKey: '{{ site.stripe_publishable_key }}',
-  chargeUrl: '{{ site.stripe_charge_url }}'
-}
