3b1e9dd66f0a9271bff35497e95003eaa8151358
Romain Gérard <romaingweb@gmail.com>
2015-04-30 17:44:31 +0200

    Revert fix for anchors and bootstrap fixed navbar

--- apps/dynamic/assets/js/modern.js
+++ apps/dynamic/assets/js/modern.js
@@ -4,9 +4,4 @@ $(document).ready(function() {
 
   // Add responsiveness to images
   $('.js-one-column img').addClass('img-responsive');
-
-  // Fix anchors scroll due to Bootstrap fixed navbar
-  var shiftWindow = function () { scrollBy(0, -75) };
-  window.addEventListener("hashchange", shiftWindow);
-  window.addEventListener("load", function () { if (window.location.hash) { shiftWindow() } });
 });
