ac4d49fcaf8c5749564427c16856a2f0c7aa0d49
Romain Gérard <romaingweb@gmail.com>
2015-04-30 18:23:13 +0200

    Restore fix for anchors and add offset options to scrollSpy

--- apps/dynamic/assets/js/modern.js
+++ apps/dynamic/assets/js/modern.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
   // Initialize scrollspy
-  $('body').scrollspy({ target: '#side-nav' });
+  $('body').scrollspy({ target: '#side-nav', offset: 85 });
 
   // Add responsiveness to images
   $('.js-one-column img').addClass('img-responsive');
+
+  // Fix anchors scroll due to Bootstrap fixed navbar
+  var shiftWindow = function () { scrollBy(0, -75) };
+  window.addEventListener("hashchange", shiftWindow);
+  window.addEventListener("load", function () { if (window.location.hash) { shiftWindow() } });
 });
