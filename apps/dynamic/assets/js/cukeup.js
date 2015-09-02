b79e54b5d1e7ce942e90374887e8d70c6c8e7707
Romain Gérard <romaingweb@gmail.com>
2015-09-02 16:38:16 +0200

    Add synopsis

--- apps/dynamic/assets/js/cukeup.js
+++ apps/dynamic/assets/js/cukeup.js
@@ -1,1 +1,8 @@
 //= require modernizr/modernizr
+//= require jquery
+
+$(function() {
+  $('.schedule-item.openable > *').on('click', function (e) {
+    $(e.currentTarget).closest('.schedule-item').toggleClass('open');
+  });
+});
