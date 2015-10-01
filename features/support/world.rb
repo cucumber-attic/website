814c5ba63fccce1f988e6dbb501551e4fc925dca
Matt Wynne <matt@mattwynne.net>
2015-10-01 08:02:15 +0100

    Namespacing

--- features/support/world.rb
+++ features/support/world.rb
@@ -1,22 +1,24 @@
 require 'cucumber/website/calendar'
 require 'cucumber/website/core/site'
 
-module World
+module Cucumber
+  module Website
+    module World
 
-  def the_ical_event
-    expect(calendars.length).to eq 1
-    expect(calendars.first.events.length).to eq 1
-    calendars.first.events.first
-  end
+      def the_ical_event
+        expect(calendars.length).to eq 1
+        expect(calendars.first.events.length).to eq 1
+        calendars.first.events.first
+      end
 
-  def the_custom_page
-    expect(custom_pages.length).to eq 1
-    custom_pages.first
-  end
+      def the_custom_page
+        expect(custom_pages.length).to eq 1
+        custom_pages.first
+      end
 
-  def create_event(attributes)
-    url = attributes[:ical_url] || 'http://lanyrd.com/2015/bdd-kickstart-san-francisco/'
-    ical_data = <<-ICAL
+      def create_event(attributes)
+        url = attributes[:ical_url] || 'http://lanyrd.com/2015/bdd-kickstart-san-francisco/'
+        ical_data = <<-ICAL
 BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//lanyrd.com//Lanyrd//EN
@@ -34,67 +36,69 @@ DTSTART;VALUE=DATE:#{attributes[:start_time].strftime("%Y%m%d")}
 DTEND;VALUE=DATE:20150424
 GEO:37.7802468;-122.3967115
 END:VEVENT
-    ICAL
+        ICAL
 
-    calendars << Cucumber::Website::FakeCalendar.new(ical_data)
-    reload_app
+        calendars << FakeCalendar.new(ical_data)
+        reload_app
+      end
+
+      def create_event_page(front_matter)
+        default_front_matter = {
+          timestamp: Chronic.parse('1 week ago'),
+          body: "<h1>#{front_matter.fetch(:title)}</h1>"
+        }
+        custom_pages << FakeEventPage.new(default_front_matter.merge(front_matter))
+        reload_app
+      end
+
+      def create_contributor(attributes = {})
+        default_attributes = {
+          avatar_url: "https://avatars.githubusercontent.com/u/#{attributes['username'].hash}"
+        }
+        Core::Contributor.with(attributes.merge(default_attributes))
+      end
+
+      def slugify(string)
+        string.gsub(' ', '-').downcase
+      end
+
+      private
+        def reload_app
+          Capybara.app = Website.make_app(site)
+          self
+        end
+
+        def site
+          @site ||= Core::Site.new(site_config, pages, calendars)
+        end
+
+        def calendars
+          @calendars ||= []
+        end
+
+        def pages
+          template_pages + custom_pages
+        end
+
+        # pages added during a test scenario
+        def custom_pages
+          @custom_pages ||= []
+        end
+
+        # hard-coded pages from our views directory
+        def template_pages
+          Page.all(site_config, views_path)
+        end
+
+        def site_config
+          Config.new('test')
+        end
+
+        def views_path
+          File.join(File.dirname(__FILE__), '..', '..', 'apps', 'dynamic', 'views')
+        end
+    end
   end
-
-  def create_event_page(front_matter)
-    default_front_matter = {
-      timestamp: Chronic.parse('1 week ago'),
-      body: "<h1>#{front_matter.fetch(:title)}</h1>"
-    }
-    custom_pages << Cucumber::Website::FakeEventPage.new(default_front_matter.merge(front_matter))
-    reload_app
-  end
-
-  def create_contributor(attributes = {})
-    default_attributes = {
-      avatar_url: "https://avatars.githubusercontent.com/u/#{attributes['username'].hash}"
-    }
-    Cucumber::Website::Core::Contributor.with(attributes.merge(default_attributes))
-  end
-
-  def slugify(string)
-    string.gsub(' ', '-').downcase
-  end
-
-  private
-    def reload_app
-      Capybara.app = Cucumber::Website.make_app(site)
-      self
-    end
-
-    def site
-      @site ||= Cucumber::Website::Core::Site.new(site_config, pages, calendars)
-    end
-
-    def calendars
-      @calendars ||= []
-    end
-
-    def pages
-      template_pages + custom_pages
-    end
-
-    # pages added during a test scenario
-    def custom_pages
-      @custom_pages ||= []
-    end
-
-    # hard-coded pages from our views directory
-    def template_pages
-      Cucumber::Website::Page.all(site_config, views_path)
-    end
-
-    def site_config
-      Cucumber::Website::Config.new('test')
-    end
-
-    def views_path
-      File.join(File.dirname(__FILE__), '..', '..', 'apps', 'dynamic', 'views')
-    end
 end
 
-World(World)
+World(Cucumber::Website::World)
