f15b14eb432b228f5b8dd15a8cedbed3640a5f8b
Romain Gérard <romaingweb@gmail.com>
2015-10-07 17:49:40 +0200

    Open only the corresponding synopsis when clicking a workshop on the CukeUp AU page

--- apps/static/assets/js/cukeup.js
+++ apps/static/assets/js/cukeup.js
@@ -2,7 +2,16 @@
 //= require jquery
 
 $(function() {
-  $('.schedule-item.openable > *').on('click', function (e) {
-    $(e.currentTarget).closest('.schedule-item').toggleClass('open');
+  $('.schedule-item.openable > .talk, .schedule-item > * > .workshop').on('click', function (e) {
+    var $currentTarget = $(e.currentTarget);
+    var $container = $currentTarget.closest('.schedule-item');
+
+    $currentTarget.find('.synopsis').toggleClass('open');
+
+    if ($container.find('.synopsis.open').length > 0)
+      $container.addClass('open');
+    else {
+      $container.removeClass('open');
+    }
   });
 });
