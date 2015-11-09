e51908717fbf12513ce2c47069638c345658a195
Steve Tooke <steve@boxjump.co.uk>
2015-11-09 08:42:43 +0000

    Add a 302 redirect to soundcloud for the podcast

--- apps/api/app.rb
+++ apps/api/app.rb
@@ -10,6 +10,8 @@ module Api
         # redirect all other api traffic to the legacy site
         # TODO: figure out a better place to host it
         r301 %r{(/api/.+)}, "http://cucumber.github.io$1"
+
+        r302 %r{/podcast/feed.xml}, "http://feeds.soundcloud.com/users/soundcloud:users:181591133/sounds.rss"
       end
     end
   end
