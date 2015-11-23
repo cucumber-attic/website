23fe47e14b1e4405a3dda0ea5c1c9cae14a75593
Romain Gérard <romaingweb@gmail.com>
2015-11-23 16:04:58 +0100

    Save event-backup and fix request specs

--- spec/requests_spec.rb
+++ spec/requests_spec.rb
@@ -21,7 +21,7 @@ describe "integration testing" do
     "/assets/main.css",
     "/fonts/cucumber-glyphs.eot",
     "/bdd-kickstart.pdf",
-    "/events/bdd-kickstart-perth-2015",
+    "/events/bdd-analysis-london-2015",
   ].each do |path|
     it "returns #{path} with a non-failure code" do
       get path
