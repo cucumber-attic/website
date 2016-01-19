daf59bccb743c10c12124d72ce5f1bd39015135c
Romain Gérard <romaingweb@gmail.com>
2016-01-19 11:38:01 +0100

    Autorotate events

--- apps/static/assets/js/modern.js
+++ apps/static/assets/js/modern.js
@@ -14,16 +14,32 @@ $(document).ready(function() {
   (function () {
     var $announcementBullets = $('.announcement-js .bullet')
     var $announcements = $('.announcement-js .announcement-text')
+    var rotationInterval
+    var rotationDelay = 5000
 
-    $announcementBullets.on('click', function (e) {
-      var $target = $(e.currentTarget)
-      var targetIndex = $announcementBullets.index($target)
+    function rotateTo($target) {
+      var index = $announcementBullets.index($target)
 
       $announcementBullets.removeClass('active')
       $target.addClass('active')
-
       $announcements.removeClass('active')
-      $announcements.eq(targetIndex).addClass('active')
+      $announcements.eq(index).addClass('active')
+    }
+
+    function startRotation() {
+      clearInterval(rotationInterval)
+      rotationInterval = setInterval(function () {
+        var $nextAnnouncement = $('.announcement-js .bullet.active').next()
+        $nextAnnouncement = $nextAnnouncement.length === 0 ? $announcementBullets.eq(0) : $nextAnnouncement
+        rotateTo($nextAnnouncement)
+      }, rotationDelay)
+    }
+
+    $announcementBullets.on('click', function (e) {
+      rotateTo($(e.currentTarget))
+      startRotation()
     })
+
+    startRotation()
   })()
 });
