39bbaa7e9b920ecd69450deab933f1f96746e961
Steve Tooke <steve@boxjump.co.uk>
2016-02-01 15:38:58 +0000

    Redirect _personal pages_ from business cards.

--- apps/redirects/app.rb
+++ apps/redirects/app.rb
@@ -7,6 +7,11 @@ module Redirects
         # redirect ruby api to rubydoc.info
         r301 %r{/api/cucumber/ruby/yardoc(.*)}, "http://www.rubydoc.info/github/cucumber/cucumber$1"
 
+        # redirect initials to homepage for tracking business cards
+        # we may want to create personal pages thanking people for chatting with us
+        # at these urls
+        r303 %r{/(ah|jb|mw|sr|st)}, "/"
+
         # redirect all other api traffic to the legacy site
         # TODO: figure out a better place to host it
         r301 %r{(/api/.+)}, "http://cucumber.github.io$1"
