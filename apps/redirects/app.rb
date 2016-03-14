5af9fb36f850479d17a7cab5043a0cc23f503652
Romain Gérard <romaingweb@gmail.com>
2016-03-14 17:45:34 +0100

    Change redirects to cucumber-ruby instead of cucumber on rubydoc

--- apps/redirects/app.rb
+++ apps/redirects/app.rb
@@ -5,7 +5,8 @@ module Redirects
     def initialize(app)
       super do
         # redirect ruby api to rubydoc.info
-        r301 %r{/api/cucumber/ruby/yardoc(.*)}, "http://www.rubydoc.info/github/cucumber/cucumber$1"
+        r301 %r{/api/cucumber/ruby/yardoc(.*)}, "http://www.rubydoc.info/github/cucumber/cucumber-ruby$1"
+        r301 %r{/cucumber/api/ruby/latest(.*)}, "http://www.rubydoc.info/github/cucumber/cucumber-ruby$1"
 
         # redirect initials to homepage for tracking business cards
         # we may want to create personal pages thanking people for chatting with us
@@ -14,6 +15,7 @@ module Redirects
 
         # redirect all other api traffic to the legacy site
         # TODO: figure out a better place to host it
+
         r301 %r{(/api/.+)}, "http://cucumber.github.io$1"
 
         r302 %r{/podcast/feed.xml}, "http://feeds.soundcloud.com/users/soundcloud:users:181591133/sounds.rss"
