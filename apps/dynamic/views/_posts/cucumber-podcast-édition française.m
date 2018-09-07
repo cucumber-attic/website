47ea54d2ac5d059958f7c54f8822652df201d31a
Theo <theo@cucumber.io>
2018-09-07 12:08:03 +0200

    French pod

---
+++ apps/dynamic/views/_posts/cucumber-podcast-édition française.m
@@ -0,0 +1,17 @@
+---
+layout:   post
+author:   theo
+title:    "édition française - Cucumber Podcast"
+date:     2018-09-07 01:00:00
+nav:      blog
+---
+<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/496504419&color=%2300cc11&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+
+Voici une édition spéciale du podcast Cucumber. En français, s’il vous plaît! Julien Biezemans, co-fondateur de Cucumber Ltd, discute BDD avec Bruno Boucard.
+
+Julien et Bruno seront à Paris pendant deux jours pour une formation sur le BDD les 11 et 12 octobre 2018 - [https://cucumber.io/events/bdd-kickstart-paris](cucumber.io/events/bdd-kickstart-paris)
+
+You can also listen and download the podcast on [iTunes](https://itunes.apple.com/gb/podcast/cucumber-podcast-rss/id1078896635), [Stitcher](http://www.stitcher.com/s?fid=81999&refid=stpr) or wherever you get your podcasts. 
+
+Join us in our [Slack channel](https://cucumber.io/support#slack) or on [Twitter](https://twitter.com/cucumberbdd)
+
